2897eb195c03075872e0aed86d5092ee7af5ce2d
Nabin <sharmanabin.0910@gmail.com>
2025-11-21 23:47:49 +0530

    Document loader

--- Semantic_Search/main.py
+++ Semantic_Search/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader    
 import os
 
 # Load from .env file
@@ -10,3 +11,17 @@ api_key = os.getenv("LANGSMITH_API_KEY")
 
 print("Tracing:", tracing)
 print("API Key:", api_key)
+
+# this was to load the pdf
+
+
+documents = [
+    Document(
+        page_content="Dogs are great companions, known for their loyalty and friendliness.",
+        metadata={"source": "mammal-pets-doc"},
+    ),
+    Document(
+        page_content="Cats are independent pets that often enjoy their own space.",
+        metadata={"source": "mammal-pets-doc"},
+    ),
+]

0f26a15ba672ca3c5a63a449aad57b1685b1d8fb
Nabin <sharmanabin.0910@gmail.com>
2025-11-22 12:50:37 +0530

    Serving requests on a backend

--- Semantic_Search/main.py
+++ Semantic_Search/main.py
@@ -1,12 +1,30 @@
-import os 
+from fastapi import FastAPI
+import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-api_key = os.getenv("LANGSMITH_API_KEY")
-tracing = os.getenv("LANGSMITH_TRACING")
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ['https://localhost:5173'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 file_path = './nke-10k-2023.pdf'
 loader = PyPDFLoader(file_path)
-
 docs = loader.load()
 
-print(len(docs))
+@app.get('/pdfdata')
+def get_data(page:int = 0):
+    doc = docs[page]
+    return{
+        'page_content' : doc.page_content,
+        'metadata' : doc.metadata,
+        'total_pages' : len(docs)
+    }
+
